d50c49601ae6836dfad3aa462c513bdef4f8946d
Valentin Chobadzhakov <valentin.chobadzhakov@gmail.com>
2019-12-03 13:47:48 +0200

    Redux connected to React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Counter from './containers/Counter/Counter';
 import './App.css';
 
@@ -7,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Counter />
+        <Counter />
       </div>
     );
   }
