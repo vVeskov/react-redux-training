7526453b96195e2b315422ec154c8713f3f594ac
Valentin Chobadzhakov <valentin.chobadzhakov@gmail.com>
2019-12-04 14:17:56 +0200

    Actions added !

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,54 +1,54 @@
-import * as actionTypes from './actions';
+// import * as actionTypes from './actions';
 
-const initialState = {
-    counter: 0,
-    results: [],
-    someVar: {
-        innerVar: 1
-    }
+// const initialState = {
+//     counter: 0,
+//     results: [],
+//     someVar: {
+//         innerVar: 1
+//     }
 
-};
+// };
 
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
 
-    switch (action.type) {
-        case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
-        case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + 5
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - 5
-            }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: state.counter })
-            }
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id,1)
-            const newArray = state.results.filter(result => result.id !== action.resultElementId)
-            return {
-                ...state,
-                results: newArray
-            }
-    }
+//     switch (action.type) {
+//         case actionTypes.INCREMENT:
+//             return {
+//                 ...state,
+//                 counter: state.counter + 1
+//             }
+//         case actionTypes.DECREMENT:
+//             return {
+//                 ...state,
+//                 counter: state.counter - 1
+//             }
+//         case actionTypes.ADD:
+//             return {
+//                 ...state,
+//                 counter: state.counter + 5
+//             }
+//         case actionTypes.SUBTRACT:
+//             return {
+//                 ...state,
+//                 counter: state.counter - 5
+//             }
+//         case actionTypes.STORE_RESULT:
+//             return {
+//                 ...state,
+//                 results: state.results.concat({ id: new Date(), value: state.counter })
+//             }
+//         case actionTypes.DELETE_RESULT:
+//             // const id = 2;
+//             // const newArray = [...state.results];
+//             // newArray.splice(id,1)
+//             const newArray = state.results.filter(result => result.id !== action.resultElementId)
+//             return {
+//                 ...state,
+//                 results: newArray
+//             }
+//     }
 
-    return state;
-}
+//     return state;
+// }
 
-export default reducer;
+// export default reducer;
