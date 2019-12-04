301c4e19d0f85a2eeea4dfc5543a434237369100
Valentin Chobadzhakov <valentin.chobadzhakov@gmail.com>
2019-12-04 12:26:56 +0200

    Combining multiply reducers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import * as actionTypes from '../../store/actions';
@@ -18,7 +18,7 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={this.props.add5ToCounter} />
                 <CounterControl label="Subtract 5" clicked={this.props.subtract5fromCounter} />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(result => (
                         <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>{result.value}</li>
@@ -31,8 +31,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     }
 }
 
@@ -42,7 +42,7 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => { dispatch({ type: actionTypes.DECREMENT }) },
         add5ToCounter: () => { dispatch({ type: actionTypes.ADD, value: 5 }) },
         subtract5fromCounter: () => { dispatch({ type: actionTypes.SUBTRACT, value: 5 }) },
-        onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+        onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
         onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElementId: id }),
     }
 }
